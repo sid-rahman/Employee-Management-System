4e7b2a8f5ee597281e7ae8bb8f9bb02b9a8757ab
sid-rahman <siddik.iitju@gmail.com>
2024-03-07 10:37:18 +0600

    deleted all

--- PracticalMVC/PracticalMVC/Models/Employee_Archieve.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PracticalMVC.Models
-{
-    public class Employee_Archieve : Employee
-    {
-        public DateTime DaletedAt { get; set; }
-    }
-}
--- PracticalMVC/PracticalMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PracticalMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
